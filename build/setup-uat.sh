4545571fff14b168f097cf135b8f608f73c884d5
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-16 03:49:07 -0400

    a

---
+++ build/setup-uat.sh
@@ -0,0 +1,7 @@
+# Get the private key from the environment variable
+echo "Setting up UAT Connection..."
+mkdir JWT
+echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
+# Authenticate to salesforce
+echo "Authenticating..."
+sfdx force: auth: logout --targetusername% USERNAME% -p sfdx force:auth:jwt:grant --clientid $SFDX_CONSUMER_KEY --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a karodrilab
