7c9dde7cce22f298598a96406ee4367f0c8c662d
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-16 14:11:06 -0400

    a

--- build/create-scratch-org.sh
+++ build/create-scratch-org.sh
@@ -5,8 +5,6 @@ echo $SFDC_SERVER_KEY | base64 -d > JWT/server.key
 
 # Authenticate to salesforce
 echo "Authenticating..."
-sfdx force:auth:jwt:grant --clientid $SFDX_CONSUMER_KEY	 --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a karodrilab
+sfdx force:auth:jwt:grant --clientid $SFDC_PROD_CLIENTID	 --jwtkeyfile JWT/server.key --username $SFDC_PROD_USER --setdefaultdevhubusername -a consultor_force+amicar@nectia.com.sbxdodoria
+ 
 
-#Create a scratch org
-echo "Creating the Scratch Org..."
-sfdx force:org:create -f config/project-scratch-def.json -a ${CIRCLE_BRANCH} -s
