871ac61ea86fcdad3ec43ed1af210c80b5dfb77a
karodrilab <83375335+karodrilab@users.noreply.github.com>
2021-06-25 09:19:29 -0400

    Update test.sh

--- build/test.sh
+++ build/test.sh
@@ -1,5 +1,6 @@
-sfdx force:auth:web:login -a $SFDC_UAT_USER -r https://test.salesforce.com/ -s
+#sfdx force:auth:web:login -a $SFDC_UAT_USER -r https://test.salesforce.com/ -s
 #sfdx force:source:push -u ${CIRCLE_BRANCH}
 sfdx force:org:list
-sfdx force:apex:test:run -n "AMICAR_ChecklistGetJSONTest" -r human -u $SFDC_UAT_USER
+sfdx force:apex:test:run -u my-hub-orgPruebasCI -w 10 -l RunSpecifiedTests -r AMICAR_ChecklistGetJSONTest -w 33 --verbose --loglevel fatal
+#my-hub-Dodoria
 
